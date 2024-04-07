89c5f7975c3d98b910bb9c53e1829beac38d9415
Admin <demo.sms143@gmail.com>
2024-04-08 01:17:42 +0530

    127:Create save Post Feature Entity class added 4th jan 2024

--- src/main/java/com/myblog/myblog11/entity/Post.java
+++ src/main/java/com/myblog/myblog11/entity/Post.java
@@ -1,9 +1,7 @@
 package com.myblog.myblog11.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name="posts")
